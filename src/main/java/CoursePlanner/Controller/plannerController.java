c30b77b04c8d4befdbd967e8a9406c4af305efd5
Richard <rxa14@sfu.ca>
2024-04-02 23:28:25 -0700

    Hard coded data path

--- src/main/java/CoursePlanner/Controller/plannerController.java
+++ src/main/java/CoursePlanner/Controller/plannerController.java
@@ -1,4 +1,6 @@
 package CoursePlanner.Controller;
+import CoursePlanner.Model.Course;
+import CoursePlanner.Model.CourseList;
 import org.springframework.web.bind.annotation.*;
 import CoursePlanner.AllApiDtoClasses.ApiAboutDTO;
 
@@ -7,10 +9,16 @@ import CoursePlanner.AllApiDtoClasses.ApiAboutDTO;
 public class plannerController {
 
     @GetMapping("/about")
-    public ApiAboutDTO haha() {
+    public ApiAboutDTO getAbout() {
         return new ApiAboutDTO("TheBestCoursePlannerEver", "Deng Chen and Richard Xiong");
-
-
     }
+
+    @GetMapping("/dump-model")
+    public void getDumpModel() {
+
+        CourseList courseList = CourseList.createTEST();
+        courseList.printAllCourses();
+    }
+
 
 }
